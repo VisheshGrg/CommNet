a10e1978154c6d3d095c586225f2361146373a9b
Vishesh Garg <gargvishesh95885@gmail.com>
2024-06-18 16:46:46 +0530

    removed unused middleware import

--- api/features/route.js
+++ api/features/route.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const authenticateToken = require("../middleware/auth");
 
 router.get("/", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
